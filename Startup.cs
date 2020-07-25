3bb4f72090b20856f288b683f0cc527961c173e5
strantr <strantr@users.noreply.github.com>
2020-07-25 17:07:52 +0100

    Added unlimited body size

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,7 @@
+using AspNetCore.Proxy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -18,6 +20,12 @@ namespace iMage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.Limits.MaxRequestBodySize = int.MaxValue; 
+            });
+
+            services.AddProxies();
             services.AddControllers();
             services.AddCors(o => o.AddPolicy("Cors", builder =>
             {
