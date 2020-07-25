5a51ea8260ca45ad9cc4a7b12dcffb7322d552a5
strantr <strantr@users.noreply.github.com>
2020-07-25 17:07:30 +0100

    Fixed watch on multiple files

--- ClientApp/build.js
+++ ClientApp/build.js
@@ -5,18 +5,14 @@ const pluginsDir = path.resolve(__dirname, "src/plugins");
 fs.readdir(pluginsDir, { withFileTypes: true }, (err, ents) => {
 	if (err) throw err;
 	ents = ents.filter((ent) => ent.isFile() && ent.name.endsWith(".ts"));
+	const args = process.argv.slice(2).join(" ");
 	const commands = ents.map(
 		(ts) =>
-			`tsc "${path.resolve(
-				pluginsDir,
-				ts.name
-			)}" --outFile "${path.resolve(
+			`tsc "${path.resolve(pluginsDir, ts.name)}" --outFile "${path.resolve(
 				__dirname,
 				"dist/plugins",
 				ts.name.substr(0, ts.name.lastIndexOf(".")) + ".js"
-			)}" --module System --target ES2020 --lib ES2015,DOM,DOM.Iterable ${process.argv
-				.splice(2)
-				.join(" ")}`
+			)}" --module System --target ES2020 --lib ES2015,DOM,DOM.Iterable ${args}`
 	);
 	concurrently(commands).catch(() => {});
 });
